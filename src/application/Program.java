2fbbb343dd9be5ba3aadc7eeb3c9c7785d9a4a08
Lucas Pratis <lucaspratis@hotmail.com>
2025-12-09 22:03:59 -0300

    Bad solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,38 +17,33 @@ public class Program {
         System.out.print("Room number: ");
         int number = sc.nextInt();
         System.out.print("Check-in date (dd/MM/yyyy): ");
-        Date checkin = sdf.parse(sc.next());
+        Date checkIn = sdf.parse(sc.next());
         System.out.print("Check-out date (dd/MM/yyyy): ");
-        Date checkinOut = sdf.parse(sc.next());
+        Date checkOut = sdf.parse(sc.next());
 
-        if (!checkinOut.after(checkin)) {
+        if (!checkOut.after(checkIn)) {
             System.out.println("Erro in reservation: Check-out date must be after check-in date");
         } else {
-            Reservation reservation = new Reservation(number, checkin, checkinOut);
+            Reservation reservation = new Reservation(number, checkIn, checkOut);
             System.out.println("Reservatio: " + reservation);
 
             System.out.println();
             System.out.println("Enter data to update the reservation: ");
 
             System.out.print("Check-in date (dd/MM/yyyy): ");
-            checkin = sdf.parse(sc.next());
+            checkIn = sdf.parse(sc.next());
             System.out.print("Check-out date (dd/MM/yyyy): ");
-            checkinOut = sdf.parse(sc.next());
+            checkOut = sdf.parse(sc.next());
 
-            Date now = new Date();
+            String error = reservation.updateDates(checkIn, checkOut);
 
-            if(checkin.before(now) || checkinOut.before(now)){
-                System.out.println("Error in reservarion: Reservation dates for update must be future");
-            } else if (!checkinOut.after(checkin)) {
-                System.out.println("Erro in reservation: Check-out date must be after check-in date");
+            if (error != null) {
+                 System.out.println("Error in reservation " + error);
             } else {
-                reservation.updateDates(checkin, checkinOut);
-                System.out.println("Reservatio: " + reservation);
+                System.out.println("Reservatio " + reservation);
             }
-
         }
 
         sc.close();
-
     }
 }
